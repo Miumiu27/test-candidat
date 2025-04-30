52e9200c913f8f320340843ff1efec771d59f3f4
Miora <kiwi.230805@gmail.com>
2025-04-30 22:10:43 +0300

    Add cors configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,18 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
 import taskRoutes from "./routes/taskRoutes";
 import authRoutes from './routes/authRoutes';
 import { authMiddleware } from './middleware/authMiddleware';
 
 const app: Application = express();
+
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || "*", 
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
