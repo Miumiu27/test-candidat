99ca7f015e4e954d82121c25e6dadf63b3325b6c
Miora <kiwi.230805@gmail.com>
2025-05-01 05:50:21 +0300

    fix: add middleware to protect routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,22 +1,23 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import taskRoutes from "./routes/taskRoutes";
-import authRoutes from './routes/authRoutes';
-import { authMiddleware } from './middleware/authMiddleware';
+import authRoutes from "./routes/authRoutes";
+import { authMiddleware } from "./middleware/authMiddleware";
 
 const app: Application = express();
 
-
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || "*", 
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks',  taskRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", authMiddleware, taskRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Bienvenue sur l'API Todo App !");
